54ffae00c34b4accaad30023e8870f7f25f61080
David Puentes <pipe01112009@gmail.com>
2026-02-26 21:14:05 -0500

    Finishing class #23.

---
+++ notes/02-typescript/11_interfaces_y_enumeraciones.md
@@ -0,0 +1,26 @@
+# Interfaces y enumeraciones
+
+## Tipos
+
+Es propio de TypeScript, consiste en datos que se permite utilizar. Por ejemplo:
+
+```tsx
+type Area = "Biology" | "Physics";
+```
+
+## Enumeraciones
+
+Consiste en otros tipos de datos que se permite utilizar en TypeScript. A diferencia de los tipos, estos se compilan a JavaScript.
+
+> **💡 Nota:** Hay que anotar que cuando `erasableSyntaxOnly` está habilitado en la configuración de TypeScript, usar enumeraciones no se permite en el lenguaje.
+
+Por ejemplo:
+
+```tsx
+enum Area2 {
+  Physics = "Physics",
+  Biology = "Biology",
+}
+
+// Ejemplo: Area.Physics;
+```
---
+++ src/data/scientist.data.ts
@@ -0,0 +1,46 @@
+type Area = "Biology" | "Physics";
+
+// Presenta problemas
+// enum Area2 {
+//   Physics = "Physics",
+//   Biology = "Biology",
+// }
+
+interface Scientist {
+  id: number;
+  name: string;
+  area: Area;
+}
+
+const scientists: Scientist[] = [
+  {
+    id: 1,
+    name: "Galilei",
+    area: "Physics",
+  },
+  {
+    id: 2,
+    name: "Mendel",
+    area: "Biology",
+  },
+  {
+    id: 3,
+    name: "Newton",
+    area: "Physics",
+  },
+  {
+    id: 4,
+    name: "Einstein",
+    area: "Physics",
+  },
+  {
+    id: 5,
+    name: "Morgan",
+    area: "Biology",
+  },
+  {
+    id: 6,
+    name: "Hooke",
+    area: "Biology",
+  },
+];
